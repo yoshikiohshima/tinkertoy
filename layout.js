56d4407e28fb8908aa907b24477478cbc18dd933
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2018-12-30 15:54:02 -0800

    put footnote at the end of section and on the right

--- layout.js
+++ layout.js
@@ -1,43 +1,49 @@
-    let imageNames;
-    let images;
+let imageNames;
+let images;
 
-    window.onresize = () => {
-	//adjust();
+function backFromFootnote() {
+    if (window.location.hash.length > 0) {
+	window.history.back();
     }
+}
 
-    window.onload = () => {
-	//imageNames = Array.from(document.getElementsByTagName("img")).filter((elem) => elem.classList.contains("right"));
-	//images = imageNames.map((img) => {
-	//return {img: img, origPercent: parseFloat(img.style.width)}
-	//});
-	//adjust();
-    }
+window.onresize = () => {
+    //adjust();
+}
 
-    adjust = () => {
-        for (let i = 0; i < images.length - 1; i++) {
-	    let data = images[i];
-            let img = data.img;
-            let nextImg = images[i + 1].img;
-	    let bottom = nextImg.previousElementSibling;
+window.onload = () => {
+    //imageNames = Array.from(document.getElementsByTagName("img")).filter((elem) => elem.classList.contains("right"));
+    //images = imageNames.map((img) => {
+    //return {img: img, origPercent: parseFloat(img.style.width)}
+    //});
+    //adjust();
+}
 
-            if (img && bottom) {
-		let imgRect = img.getBoundingClientRect();
-		let origWidth = window.innerWidth * (data.origPercent / 100);
-		let origScale = origWidth / img.naturalWidth
-		let imgOrigHeight = img.naturalHeight * origScale;
-		let imgOrigBottom = img.offsetTop + imgOrigHeight;
+adjust = () => {
+    for (let i = 0; i < images.length - 1; i++) {
+	let data = images[i];
+        let img = data.img;
+        let nextImg = images[i + 1].img;
+	let bottom = nextImg.previousElementSibling;
 
-		let bottomRect = bottom.getBoundingClientRect();
-		let bottomBottom = bottom.offsetTop + bottomRect.height;
+        if (img && bottom) {
+	    let imgRect = img.getBoundingClientRect();
+	    let origWidth = window.innerWidth * (data.origPercent / 100);
+	    let origScale = origWidth / img.naturalWidth
+	    let imgOrigHeight = img.naturalHeight * origScale;
+	    let imgOrigBottom = img.offsetTop + imgOrigHeight;
 
-		if (imgOrigBottom > bottomBottom) {
-		    let excess = imgOrigBottom - bottomBottom + 10;
-		    let ratio = (imgOrigHeight - excess) / imgOrigHeight;
-		    let newPercent = data.origPercent * ratio;
-		    img.style.width = newPercent.toFixed(2) + "%";
-		} else {
-		    img.style.width = data.origPercent + "%";
-		}
-            }
+	    let bottomRect = bottom.getBoundingClientRect();
+	    let bottomBottom = bottom.offsetTop + bottomRect.height;
+
+	    if (imgOrigBottom > bottomBottom) {
+		let excess = imgOrigBottom - bottomBottom + 10;
+		let ratio = (imgOrigHeight - excess) / imgOrigHeight;
+		let newPercent = data.origPercent * ratio;
+		img.style.width = newPercent.toFixed(2) + "%";
+	    } else {
+		img.style.width = data.origPercent + "%";
+	    }
         }
     }
+}
