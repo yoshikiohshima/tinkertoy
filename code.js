b7d4781119d7aeccb8b62ba2e8d3d6ed55075fa1
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2019-04-16 09:59:38 -0700

    try to adapt to the new go.html

--- code.js
+++ code.js
@@ -15,24 +15,7 @@ function detectMobile() {
         || navigator.userAgent.match(/iPod/i)
         || navigator.userAgent.match(/BlackBerry/i)
         || navigator.userAgent.match(/Windows Phone/i));
-};
-
-// window.onscroll = evt => {
-//     let docViewTop = window.scrollY;
-//     let docViewBottom = docViewTop + window.innerHeight;
-
-//     Object.entries(gps).forEach(([key, gp]) => {
-//         let elemTop = gp.iframe.offsetTop;
-//         let elemBottom = elemTop + gp.iframe.offsetHeight;
-//         let visible = elemTop < docViewBottom && docViewTop < elemBottom;
-//         if (gp.running !== visible) {
-//             console.log(gp.iframe.id, "switch to: ", visible);
-//             let message = visible ? "resume GP" : "suspend GP";
-//             gp.running = visible;
-//             gp.iframe.contentWindow.postMessage(message, '*');
-//         }
-//     });
-// };
+}
 
 window.onload = () => {
     holder = document.getElementById("loaderholder");
@@ -46,6 +29,7 @@ window.onload = () => {
     } else {
         addGP("afterWanderAndAvoid", "wanderAndAvoid.gpp", w, h);
         addGPLauncher("afterNudibranchNeuronChain", "NudibranchNeuronChain.gpp", w, h);
+
     }
 
     stillLoading = true;
@@ -79,7 +63,7 @@ function addGP(after, projectName, width, height, optReplacementopt) {
     if (optReplacementopt) {
         parent.removeChild(optReplacementopt);
     }
-    //div.style.setProperty("display", "none");
+    div.style.setProperty("display", "none");
     parent.insertBefore(div, prev);
     gps[projectName].loading = () => {
         gps[projectName].div.style.removeProperty("display");
@@ -122,7 +106,7 @@ function makeGP(after, projectName, width, height) {
     div.classList.add("middle");
     div.style.height = height;
     div.style.width = width;
-    
+
     div.appendChild(iframe);
 
     gps[projectName] = {
@@ -138,15 +122,7 @@ function makeGPLauncher(after, projectName, width, height) {
     launcher.style.height = height;
     launcher.classList.add("launcher");
 
-    //let button = document.createElement("img");
-    //button.src = "round_blue_play_button_down.svg";
-
-    //button.classList.add("launcherButton");
-
     launcher.style.setProperty("background-image", `url(${projectName + "-background.png"})`);
-    //let buttonHolder = document.createElement("div");
-    //buttonHolder.style.height = "220px";
-    //buttonHolder.style.width = "220px";
 
     launcher.style.setProperty("background", `linear-gradient(rgba(0,0,0,0.5), rgba(0, 0, 0, 0.5) ),url(${projectName + "-background.png"}) no-repeat`);
     launcher.style.setProperty("background-size", "100%");
@@ -154,19 +130,13 @@ function makeGPLauncher(after, projectName, width, height) {
     let msg = document.createElement("div");
     msg.innerHTML = document.getElementById("launcherMessage").innerHTML;
 
-`
-Tap here to start the dynamic content.<BR>
-It may take up to a minute to launch.`;
-
     msg.classList.add("launcherMessage");
-
-    //buttonHolder.appendChild(button);
-    //launcher.appendChild(buttonHolder);
 
     launcher.appendChild(msg);
 
     launcher.onclick =() => {
-        addGP(after, projectName, width, height, launcher);
+        let div = addGP(after, projectName, width, height, launcher);
+        div.style.removeProperty("display"); // take this line out
         console.log("click", projectName);
     };
 
@@ -188,6 +158,13 @@ function loaderAnimation() {
         setTimeout(loaderAnimation, 600);
     } else {
         holder.innerText = "";
+        // just to make sure things get started
+        for (let k in gps) {
+            if (gps[k].loading) {
+                gps[k].loading();
+                gps[k].loading = null;
+            }
+        }
     }
 }
 
@@ -228,4 +205,22 @@ function moveGP(projectName) {
         gp.iframe.classList.remove("loading");
     }
 }
+
+window.onscroll = evt => {
+    let docViewTop = window.scrollY;
+    let docViewBottom = docViewTop + window.innerHeight;
+
+    Object.entries(gps).forEach(([key, gp]) => {
+        let elemTop = gp.iframe.offsetTop;
+         let elemBottom = elemTop + gp.iframe.offsetHeight;
+         let visible = elemTop < docViewBottom && docViewTop < elemBottom;
+         if (gp.running !== visible) {
+             console.log(gp.iframe.id, "switch to: ", visible);
+             let message = visible ? "resume GP" : "suspend GP";
+             gp.running = visible;
+             gp.iframe.contentWindow.postMessage(message, '*');
+         }
+     });
+ };
 */
+
